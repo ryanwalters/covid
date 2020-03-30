c0eaabb676b1e24c6e32b3591818caaeed22c4d2
Ryan Walters <shout at ryanwalters dot co>
2020-03-30 01:55:35 -0500

    add hospitalized and deaths charts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import dayjs from "dayjs";
-import React, { Fragment, useEffect, useRef, useState } from "react";
-import fetch from "node-fetch";
-import Head from "next/head";
+import dayjs from 'dayjs';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import fetch from 'node-fetch';
+import Head from 'next/head';
 import {
   Crosshair,
+  DiscreteColorLegend,
   Hint,
   HorizontalGridLines,
   LineSeries,
@@ -11,8 +12,8 @@ import {
   XAxis,
   XYPlot,
   YAxis,
-} from "react-vis";
-import { Navbar } from "reactstrap";
+} from 'react-vis';
+import { Navbar } from 'reactstrap';
 
 const Home = ({ data, graphs }) => {
   const containerRef = useRef(null);
@@ -34,7 +35,7 @@ const Home = ({ data, graphs }) => {
   }, [containerRef.current]);
 
   function onNearestXY(value, data) {
-    //console.log(value, data);
+    console.log(value, data);
   }
 
   function transformData(data) {
@@ -51,51 +52,46 @@ const Home = ({ data, graphs }) => {
       </Head>
       <Navbar color="dark">US COVID-19 Cases</Navbar>
       <div className="container mb-5" ref={containerRef}>
-        {graphs.map(({ heading, subHeading, yLabel, data }) => (
+        {graphs.map(({ heading, subHeading, yLabel, positive, hospitalized, death }) => (
           <Fragment key={heading}>
             <h3 className="mt-3">{heading}</h3>
             <div className="font-weight-light">{subHeading}</div>
             <XYPlot width={width} height={400} margin={{ left: 60 }}>
               <HorizontalGridLines style={{ opacity: 0.1 }} />
-              <XAxis
-                title="Date"
-                tickFormat={(tick) => dayjs(tick).format("MMM D")}
-              />
+              <XAxis title="Date" tickFormat={(tick) => dayjs(tick).format('MMM D')} />
               <YAxis title={yLabel} />
-              <LineSeries
-                data={transformData(data)}
-                curve="curveBasis"
-                onNearestXY={onNearestXY}
-              />
+              <LineSeries data={transformData(positive)} curve="curveBasis" onNearestXY={onNearestXY} />
+              <LineSeries data={transformData(hospitalized)} curve="curveBasis" />
+              <LineSeries data={transformData(death)} curve="curveBasis" stroke="burlywood" />
               {/*<Crosshair values={data} itemsFormat={items => items.map(({ positive: value }) => ({ value }))} />*/}
-              {/*<Hint value={data} format={items => items.map(item => ({ title: dayjs(item.dateChecked).format('MMM D'), value: item.positive }))} />*/}
             </XYPlot>
+            <DiscreteColorLegend items={['Positive Cases', 'Hospitalized', { title: 'Deaths', color: 'burlywood' }]} />
           </Fragment>
         ))}
-        {process.env.NODE_ENV !== "production" && (
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        )}
+        {process.env.NODE_ENV !== 'production' && <pre>{JSON.stringify(data, null, 2)}</pre>}
       </div>
     </div>
   );
 };
 
 export async function getServerSideProps(context) {
-  const data = await fetch(
-    "https://covidtracking.com/api/us/daily"
-  ).then((response) => response.json());
+  const data = await fetch('https://covidtracking.com/api/us/daily').then((response) => response.json());
   const graphs = [
     {
-      heading: "Cumulative",
-      subHeading: "Total number of confirmed cases",
-      yLabel: "Total Cases",
-      data: data.map(({ dateChecked: x, positive: y }) => ({ x, y })),
+      heading: 'Cumulative',
+      subHeading: 'Total number of cases',
+      yLabel: 'Total Cases',
+      positive: data.map(({ dateChecked: x, positive: y }) => ({ x, y })),
+      hospitalized: data.map(({ dateChecked: x, hospitalized: y }) => ({ x, y })),
+      death: data.map(({ dateChecked: x, death: y }) => ({ x, y })),
     },
     {
-      heading: "Day-over-day",
-      subHeading: "New confirmed cases since the previous day",
-      yLabel: "New Cases",
-      data: data.map(({ dateChecked: x, positiveIncrease: y }) => ({ x, y })),
+      heading: 'Day-over-day',
+      subHeading: 'New cases since the previous day',
+      yLabel: 'New Cases',
+      positive: data.map(({ dateChecked: x, positiveIncrease: y }) => ({ x, y })),
+      hospitalized: data.map(({ dateChecked: x, hospitalizedIncrease: y }) => ({ x, y })),
+      death: data.map(({ dateChecked: x, deathIncrease: y }) => ({ x, y })),
     },
   ];
 
