0166605c9dd7a99670fdb879e037965306b47464
Ryan Walters <shout at ryanwalters dot co>
2020-03-31 00:40:15 -0500

    lighthouse seo audit: add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en-us">
         <Head>
+          <meta
+            name="description"
+            content="A visual representation of the status of the total and day-over-day coronavirus (COVID-19) cases in the United States"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://www.google-analytics.com" />
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-998540-9" />
